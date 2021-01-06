102250b9f97a7a8f23e92339ed06152a9921f321
denghaifeng <denghaifeng@innotechx.com>
2021-01-06 16:55:43 +0800

    删除无用的函数

--- targets/target_functions.py
+++ targets/target_functions.py
@@ -1,13 +1,10 @@
 import numpy as np
 import cv2
 
-def brightness_adjust(image, delta):
-    image[:,:,2] = np.clip(image[:,:,2] * (1.0 + delta), 0, 255)
-    return image
 
 def histogram_equal(image, index):
     a, b, c = cv2.split(image)
-    if index == 0: #h
+    if index == 0:  # h
         equ = cv2.equalizeHist(a)
         res = np.hstack((a, equ))
         image = cv2.merge((a, b, equ))
@@ -21,16 +18,3 @@ def histogram_equal(image, index):
         image = cv2.merge((a, b, equ))
 
     return image
-
-def bgr2hsv(image):
-    cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    return image
-
-def unsharp_mask(image, sigmaX, weight):
-    blur_img = cv2.GaussianBlur(image, (0, 0), sigmaX)
-    image = cv2.addWeighted(image, 1.0 + weight, blur_img, -weight, 0)
-
-    return image
-
-def nothing(x):
-    pass
