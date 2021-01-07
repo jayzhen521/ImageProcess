4a4fcf1f95034a683d3a6f220c9eb470f320dade
denghaifeng <denghaifeng@innotechx.com>
2021-01-07 17:58:25 +0800

    添加导向滤波功能

--- processor/unsharpenmask.py
+++ processor/unsharpenmask.py
@@ -30,3 +30,30 @@ class unsharpenmask:
         blur = cv2.GaussianBlur(
             rgb, (self.ksize, self.ksize), self.sigma, self.sigma)
         return cv2.addWeighted(rgb, 1.0 + self.weight, blur, -self.weight, 0)
+
+    def GuideBlur(self, I, p, winSize, eps):
+        mean_I = cv2.blur(I, winSize)      # I的均值平滑
+        mean_p = cv2.blur(p, winSize)      # p的均值平滑
+
+        mean_II = cv2.blur(I * I, winSize)  # I*I的均值平滑
+        mean_Ip = cv2.blur(I * p, winSize)  # I*p的均值平滑
+
+        var_I = mean_II - mean_I * mean_I  # 方差
+        cov_Ip = mean_Ip - mean_I * mean_p  # 协方差
+
+        a = cov_Ip / (var_I + eps)         # 相关因子a
+        b = mean_p - a * mean_I            # 相关因子b
+
+        mean_a = cv2.blur(a, winSize)      # 对a进行均值平滑
+        mean_b = cv2.blur(b, winSize)      # 对b进行均值平滑
+
+        q = mean_a * I + mean_b
+        return q
+
+    def do_guide_usm(self, rgb):
+        I = rgb / 255.0
+
+        blur = np.clip(self.GuideBlur(I, I, (self.ksize, self.ksize), 0.01)
+                       * 255.0, 0, 255.0).astype(np.uint8)
+
+        return cv2.addWeighted(rgb, 1.0 + self.weight, blur, -self.weight, 0)
